563dc1e84206ffee554970465bfebdc1d402d240
RBB-TECHNIK-PC-\Schueler-ADM <ossenschmidtphoebe@web.de>
2024-03-18 09:42:40 +0100

    ADD test data, function, readme

--- src/main/java/com/example/unittests/HelloApplication.java
+++ src/main/java/com/example/unittests/HelloApplication.java
@@ -17,6 +17,34 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    protected double estimateFee(int speedingValue) {
+        double result = 0;
+
+        if (0 < speedingValue && speedingValue <= 10) {
+            result = 48.50;
+        } else if (11 <= speedingValue && speedingValue <= 15) {
+            result = 68.50;
+        } else if (16 <= speedingValue && speedingValue <= 20) {
+            result = 88.50;
+        } else if (21 <= speedingValue && speedingValue <= 25) {
+            result = 128.50;
+        } else if (26 <= speedingValue && speedingValue <= 30) {
+            result = 178.50;
+        } else if (31 <= speedingValue && speedingValue <= 40) {
+            result = 228.50;
+        } else if (41 <= speedingValue && speedingValue <= 50) {
+            result = 348.50;
+        } else if (51 <= speedingValue && speedingValue <= 60) {
+            result = 508.50;
+        } else if (61 <= speedingValue && speedingValue <= 70) {
+            result = 633.50;
+        } else if (speedingValue > 70) {
+            result = 738.50;
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         launch();
     }
