4a55c66488fdc64d04d0ebaa9511bce2fa122644
GINAS-LAPTOP\gina3 <g.jenge@gmx.de>
2024-03-18 13:30:16 +0100

    Funktionen in GUI

--- src/main/java/com/example/unittests/HelloApplication.java
+++ src/main/java/com/example/unittests/HelloApplication.java
@@ -3,11 +3,15 @@ package com.example.unittests;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
--- src/main/java/com/example/unittests/HelloController.java
+++ src/main/java/com/example/unittests/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.unittests;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField speed;
+    @FXML
+    private Label punish;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void calculate() {
+
+        String speedInput = speed.getText(); // Abrufen des eingegebenen Werts
+        int speed = Integer.parseInt(speedInput); // Konvertierung in einen Integer
+
+
+        Fees fees1 = new Fees();
+        Double speedy = fees1.estimateFee(speed);
+        String money = String.valueOf(speedy);
+        String money2 = money.replace('.', ',');
+        punish.setText(money2 + "0 €");
+
     }
 }
