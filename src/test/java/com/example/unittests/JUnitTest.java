aa6580bd1e208aa3209368761135b9094571c325
RBB-TECHNIK-PC-\Schueler-ADM <philipp-rupp2000@web.de>
2024-03-18 09:20:10 +0100

    add junit package and class

---
+++ src/test/java/com/example/unittests/JUnitTest.java
@@ -0,0 +1,11 @@
+package com.example.unittests;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    @Test
+    void testBußgeldkatalog1 () {
+        int testSpeed = 10;
+        double expectedResult = 48.50;
+    }
+}
