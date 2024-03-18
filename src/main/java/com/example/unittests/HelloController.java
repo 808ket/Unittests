9ff4958140b483fc818592687bc12e6e70737025
808ket <ketelhohnkilian@gmail.com>
2024-03-18 09:58:20 +0100

    parameter übergabe an fees

--- src/main/java/com/example/unittests/HelloController.java
+++ src/main/java/com/example/unittests/HelloController.java
@@ -2,13 +2,28 @@ package com.example.unittests;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextField speedTextField; // Referenz zur TextField-Komponente
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        System.out.println("Funktioniert");
+
+        String speedInput = speedTextField.getText(); // Abrufen des eingegebenen Werts
+        int speed = Integer.parseInt(speedInput); // Konvertierung in einen Integer
+
+        // Verarbeite den eingegebenen Wert weiter...
+        System.out.println("Eingegebene Geschwindigkeit: " + speed);
+
+        Fees fees1 = new Fees();
+        System.out.println( fees1.estimateFee(speed));
+        fees1.estimateFee(speed);
     }
 }
