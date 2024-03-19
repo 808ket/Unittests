a63ea25845f974eb835e0cb83d95696db9b852ff
GINAS-LAPTOP\gina3 <g.jenge@gmx.de>
2024-03-19 10:16:00 +0100

    Abfrage nummersiche Eingabe

--- src/main/java/com/example/unittests/HelloController.java
+++ src/main/java/com/example/unittests/HelloController.java
@@ -12,14 +12,19 @@ public class HelloController {
     private TextField speed;
     @FXML
     private Label punish;
+    @FXML
+    private Label alert;
 
     @FXML
     protected void calculate() {
 
         String speedInput = speed.getText(); // Abrufen des eingegebenen Werts
+        if(speedInput.matches("[A-Za-z]")) {
+            alert.setText("Bitte nur Zahlen eingeben!");
+        }else{
+            alert.setVisible(false);
+        }
         int speed = Integer.parseInt(speedInput); // Konvertierung in einen Integer
-
-
         Fees fees1 = new Fees();
         Double speedy = fees1.estimateFee(speed);
         String money = String.valueOf(speedy);
